4ceb4aa74a27ac96ca70cd6e3dea70b94c1a0820
Lucas Holmes <lucas.holmes@backslash.build>
2023-09-01 13:00:05 +0100

    Debug test suite

--- react-testing-project/src/App.test.js
+++ react-testing-project/src/App.test.js
@@ -1,19 +1,24 @@
 import React from "react";
 import { render, screen } from "@testing-library/react";
 import "@testing-library/jest-dom";
+
+import userEvent from "@testing-library/user-event";
+import { act } from "react-dom/test-utils";
 import App from "./App";
 
 describe("App component", () => {
   test("Has valid header", () => {
     render(<App />);
-    expect(screen.getByRole("heading").toHaveTextContent("TDD Test Header"));
+    expect(screen.getByRole("heading")).toHaveTextContent("TDD Test Header");
   });
 
   test("Has valid heading update action", () => {
     render(<App />);
     const button = screen.getByRole("button", { name: "Update Heading" });
 
-    userEvent.click(button);
+    act(() => {
+      userEvent.click(button);
+    });
 
     expect(screen.getByRole("heading")).toHaveTextContent("New TDD Test Header");
   });
